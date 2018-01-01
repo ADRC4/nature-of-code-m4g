34ea1f230147a84af96772c40d0414183c9ae226
XManNguyen <34646216+XManNguyen@users.noreply.github.com>
2018-01-01 01:51:52 +0000

    Add files via upload

---
+++ Assets/NeuralNetwork/10.2_Perceptron/Perceptron.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perceptron
+{
+    //The activation function
+    public int sign(float n)
+    {
+        if (n > 0) return 1;
+        else return -1;
+    }
+
+    float[] weights = new float[2];  
+    
+    //Constructor
+    public void Percetron()
+    {
+        //Initialize the weights randomly
+        for (int i = 0; i < weights.Length; i++)
+        {
+            weights[i] = Random.Range(-1, 1);
+        }
+        
+    }
+    
+    public int guess (float[] inputs)
+    {
+        float sum = 0;
+        for (int i = 0; i < weights.Length ; i++)
+        {
+            sum += inputs[i] * weights[i];
+        }
+
+        int output = sign(sum);
+        return output;
+    }
+}
---
+++ Assets/NeuralNetwork/10.2_Perceptron/SimplePerceptron.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePerceptron : MonoBehaviour
+{
+    private GUISkin _skin;
+    private Rect _rectangle;
+    private int _width;
+    private int _height;
+    private int _size;
+    private Texture2D _image;
+    private Color[] _colors;
+
+    Point[] points = new Point[100];
+    Perceptron p;
+
+	void Start ()
+    {
+        //GUI Layout
+        int scale = 4;
+        _width = Screen.width / scale;
+        _height = Screen.height / scale;
+        _size = _width * _height;
+        _image = new Texture2D(_width, _height);
+        _rectangle = new Rect(0, 0, Screen.width, Screen.height);
+        _colors = new Color[_size];
+
+        //Perceptron Logic
+        p = new Perceptron();
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = new Point();
+        }
+
+
+        float[] inputs = { 1, 0.5f };
+        int guess = p.guess(inputs);
+        print(guess);
+
+	}
+
+    void Draw()
+    {
+        foreach (Point p in points)
+        {
+            p.Show();
+
+            _image.SetPixels(_colors);
+            _image.Apply();
+        }
+    }
+
+
+    private void OnGUI()
+    {
+        GUI.skin = _skin;
+        GUI.DrawTexture(_rectangle, _image);       
+    }
+}
