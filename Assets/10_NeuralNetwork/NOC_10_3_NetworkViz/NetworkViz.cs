bb3eeb2332b9c910671cbb1751b6a70949403dc3
XManNguyen <34646216+XManNguyen@users.noreply.github.com>
2018-01-07 14:27:02 +0000

    SeekingNeuralCopyJavascript

--- Assets/10_NeuralNetwork/NOC_10_3_NetworkViz/NetworkViz.cs
+++ Assets/10_NeuralNetwork/NOC_10_3_NetworkViz/NetworkViz.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class NetworkViz : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Network network;
+
+    void setup()
+    {
+        size(640, 360);
+        // Create the Network object
+        network = new Network(width / 2, height / 2);
+
+        // Create a bunch of Neurons
+        Neuron a = new Neuron(-200, 0);
+        Neuron b = new Neuron(0, 75);
+        Neuron c = new Neuron(0, -75);
+        Neuron d = new Neuron(200, 0);
+
+        // Connect them
+        network.connect(a, b);
+        network.connect(a, c);
+        network.connect(b, d);
+        network.connect(c, d);
+
+        // Add them to the Network
+        network.addNeuron(a);
+        network.addNeuron(b);
+        network.addNeuron(c);
+        network.addNeuron(d);
+    }
+
+    void draw()
+    {
+        background(255);
+        // Draw the Network
+        network.display();
+        noLoop();
+    }
 }
