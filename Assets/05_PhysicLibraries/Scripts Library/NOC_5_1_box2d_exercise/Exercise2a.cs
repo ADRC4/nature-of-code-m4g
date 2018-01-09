b8b467a1ba4dbe1e2ee725f3f720b93cc7992f25
Mengyu Huang <mengyu.huang@ucl.ac.uk>
2018-01-09 10:52:11 +0000

    First four exercises of Chapter 5.

---
+++ Assets/05_PhysicLibraries/Scripts Library/NOC_5_1_box2d_exercise/Exercise2a.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise2a : MonoBehaviour {
+
+    public GameObject Box;
+    public static List<GameObject> Boxes = new List<GameObject>();
+    private float _x;
+    private float _y;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var newBox = Instantiate(Box);
+            Boxes.Add(newBox);
+        }
+    }
+}
+
+
+
+
+
+
