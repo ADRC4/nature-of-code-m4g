4c40311fc1ebb1c7356c6af1c2518a550e5b760d
Mengyu Huang <mengyu.huang@ucl.ac.uk>
2018-01-11 00:59:26 +0000

    Add 'GravityBox' class and try to call it in Exercise2b, but it seems that there are something wrong in GravityBox.

--- Assets/05_PhysicLibraries/Scripts Library/NOC_5_2_Boxes/Exercise_5_2.cs
+++ Assets/05_PhysicLibraries/Scripts Library/NOC_5_2_Boxes/Exercise_5_2.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Exercise_5_2 : MonoBehaviour
+public class Exercise2b : MonoBehaviour
 {
     public GameObject Box;
     public GameObject Platform;
     public static List<GameObject> Boxes = new List<GameObject>();
-    public GravityBox gb;
+
+    //void Start()
+    //{
+    //    Physics.gravity = new Vector3(0, -1.0F, 0);
+    //}
 
     void Update()
     {
@@ -21,7 +24,8 @@ public class Exercise_5_2 : MonoBehaviour
 
             var newBox = Instantiate(Box, pos, rotation);
             Boxes.Add(newBox);
-            gb.Move(Boxes);
+
+            //if(Platform)
         }
     }
 }
