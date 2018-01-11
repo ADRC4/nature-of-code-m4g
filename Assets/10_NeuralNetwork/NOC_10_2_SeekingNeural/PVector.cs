20fd6eeb341643fbeb89897c33a4fd94111dbbfa
wenjizhang <34709971+wenjizhang@users.noreply.github.com>
2018-01-11 21:10:32 +0000

    seeking neural update

--- Assets/10_NeuralNetwork/NOC_10_2_SeekingNeural/PVector.cs
+++ Assets/10_NeuralNetwork/NOC_10_2_SeekingNeural/PVector.cs
@@ -4,72 +4,125 @@ using UnityEngine;
 
 public class PVector
 {
+    public float x { set; get; }
+    public float y { set; get; }
 
-    public float x;
-    public float y;
-
-    public PVector(float x_, float y_)
+    public PVector()
     {
-        x = x_;
-        y = y_;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public PVector(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
     }
 
     public void add(PVector v)
     {
+        x += v.x;
+        y += v.y;
+    }
 
-        y = y + v.y;
-
-        x = x + v.x;
+    public void add2(PVector v, PVector w)
+    {
+        x = v.x + w.x;
+        y = v.y + w.y;
     }
 
     public void sub(PVector v)
     {
-
-        y = y - v.y;
-
         x = x - v.x;
+        y = y - v.y;
     }
 
-    public void sub2(PVector v, PVector w) ///----- 2 arguments
+    public void sub2(PVector v, PVector w)
     {
         x = v.x - w.x;
         y = v.y - w.y;
-        
     }
-
 
     public void mult(float n)
     {
+
         x = x * n;
         y = y * n;
-    }
 
-    public void div (float n)
+    }
+    public void limit(float f)
     {
-        x = x / n;
-        y = y / n;
-    }
 
-    public float mag()
-    {
-        return Mathf.Sqrt(x * x + y * y);
-    }
 
+        if ((double)this.sqrMagnitude > (double)f * (double)f)
+            this.set(this.normalized * f);
+
+
+    }
     public void normalize()
     {
-        float m = mag();
-        if (m != 0)
+        float magnitude = this.magnitude;
+        if ((double)magnitude > 9.99999974737875E-06)
+            this.set(this / magnitude);
+        else
         {
-            div(m);
+            this.x = 0;
+            this.y = 0;
         }
     }
-    
-    //public void limit(float f)
-    //{
-    //    if ((double)this.sqrMagnitude > (double)f * (double)f)
-    //        this.set(this.normalized * f);
-    //}
 
+    public PVector normalized
+    {
+        get
+        {
+            PVector PVector = new PVector(this.x, this.y);
+            PVector.normalize();
+            return PVector;
+        }
+    }
+
+    public void set(PVector v)
+    {
+        x = v.x;
+        y = v.y;
+    }
+
+    public float magnitude
+    {
+        get
+        {
+            return Mathf.Sqrt((float)((double)this.x * (double)this.x + (double)this.y * (double)this.y));
+        }
+    }
+
+    public float sqrMagnitude
+    {
+        get
+        {
+            return (float)((double)this.x * (double)this.x + (double)this.y * (double)this.y);
+        }
+    }
+
+    public static PVector operator /(PVector a, float d)
+    {
+        return new PVector(a.x / d, a.y / d);
+    }
+
+    public static PVector operator +(PVector a, PVector b)
+    {
+        return new PVector(a.x + b.x, a.y + b.y);
+    }
+
+    public static PVector operator *(PVector a, float d)
+    {
+        return new PVector(a.x * d, a.y * d);
+    }
+
+
+    public static implicit operator Vector3(PVector v)
+    {
+        return new Vector3(v.x, v.y, 0.0f);
+    }
 
 }
 
--- Assets/10_NeuralNetwork/NOC_10_2_SeekingNeural/SeekingNeural.cs
+++ Assets/10_NeuralNetwork/NOC_10_2_SeekingNeural/SeekingNeural.cs
@@ -1,82 +1,85 @@
-﻿//using UnityEngine;
-//using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-//public class SeekingNeural : MonoBehaviour
-//{
-//    Vehicle v;
+public class SeekingNeural : MonoBehaviour
+{
+    Vehicle v;
 
-//    PVector desired;
+    Vector2 desired;
+    Vector3 target;
 
-//    PVector[] targets;
+    List<Vector2> targets;
+    public float width = 6.4f;
+    public float height = 3.6f;
 
-//    void setup()
-//    {
-//        size(640, 360);
-//        // The Vehicle's desired position
-//        desired = new PVector(Screen.width / 2, Screen.height / 2);
+    void Start()
+    {
+        // The Vehicle's desired position
+        desired = new Vector2(0, 0);
 
+        // Create a list of targets
+        MakeTargets();
 
-//        // Create a list of targets
-//        makeTargets();
+        // Create the Vehicle (it has to know about the number of targets
+        // in order to configure its brain)
+        v = new Vehicle(targets.Count, Random.Range(0, width), Random.Range(0, height));
+    }
 
-//        // Create the Vehicle (it has to know about the number of targets
-//        // in order to configure its brain)
-//        v = new Vehicle(targets.Length(), Random.Range(0,Screen.width), Random.Range(0,Screen.height));
-//    }
+    // Make a random ArrayList of targets to steer towards
+    void MakeTargets()
+    {
+        targets = new List<Vector2>();
+        for (int i = 0; i < 8; i++)
+        {
+            target = new Vector3 (Random.Range(0, width), Random.Range(0, height), 0);
+            targets.Add(target);
+        }
+    }
 
-//    // Make a random ArrayList of targets to steer towards
-//    void makeTargets()
-//    {
-//        targets = new PVector[i];
-//        for (int i = 0; i < 8; i++)
-//        {
-//            targets.add(new PVector(Random.Range(0, Screen.width), Random.Range(0, Screen.height)));
-//        }
-//    }
+    void OnDrawGizmos()
+    {
+        //background(255);
 
-//    void OnDrawGizmos ()
-//    {
-//        //background(255);
+        // Draw a circle to show the Vehicle's goal
+        //stroke(0);
+        //strokeWeight(2);
+        //fill(0, 100);
+        //ellipse(desired.x, desired.y, 36, 36);
 
-//        // Draw a circle to show the Vehicle's goal
-//        //stroke(0);
-//        //strokeWeight(2);
-//        //fill(0, 100);
-//        //ellipse(desired.x, desired.y, 36, 36);
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(new Vector3(desired.x, desired.y,0), new Vector3(0.6f,0.6f,0.6f));
 
-//        Gizmos.color = Color.blue;
-//        Gizmos.DrawWireSphere(new Vector3(desired.x, desired.y, 0), 0.3f);
+        // Draw the targets
+        foreach (Vector3 t in targets)
+        {
+            //noFill();
+            //stroke(0);
+            //strokeWeight(2);
 
-//        // Draw the targets
-//        foreach (PVector[] target in targets)
-//        {
-//            //noFill();
-//            //stroke(0);
-//            //strokeWeight(2);
+            Gizmos.DrawWireSphere(new Vector3(target.x, target.y, 0), 0.2f);
 
-//            Gizmos.DrawWireSphere(new Vector3(target.x, target.y, 0), 0.1f);
+            var p1 = new Vector3(target.x, target.y - 0.016f, 0);
+            var p2 = new Vector3(target.x - 0.016f, target.y, 0);
+            Gizmos.DrawLine(p1, p2);
 
+            //ellipse(target.x, target.y, 16, 16);
+            //line(target.x, target.y - 16, target.x, target.y + 16);
+            //line(target.x - 16, target.y, target.x + 16, target.y);
+        }
 
-//            var p1 = new Vector3 (target.x, target.y - 16, 0);
-//            var p2 = new Vector3 (target.x - 16, target.y, 0);
-//            Gizmos.DrawLine(p1, p2);
+        // Update the Vehicle
+        v.steer(targets);
+        v.Update();
+        v.Display();
+    }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MakeTargets();
+        }
+    }
 
-
-//            //ellipse(target.x, target.y, 16, 16);
-//            //line(target.x, target.y - 16, target.x, target.y + 16);
-//            //line(target.x - 16, target.y, target.x + 16, target.y);
-//        }
-
-//        // Update the Vehicle
-//        v.steer(targets);
-//        v.Update();
-//        v.Display();
-//    }
-
-//    void OnMouseButton()
-//    {
-//        makeTargets();
-//    }
-
-//}
+}
