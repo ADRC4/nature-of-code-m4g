684ce680deb307888aded9b2c38d4530d98c4797
XManNguyen <34646216+XManNguyen@users.noreply.github.com>
2018-01-16 19:05:07 +0000

    Final Update

--- Assets/10_NeuralNetwork/NOC_10_1_SimplePerceptron/SimplePerceptronP2.cs
+++ Assets/10_NeuralNetwork/NOC_10_1_SimplePerceptron/SimplePerceptronP2.cs
@@ -17,8 +17,8 @@ public class SimplePerceptronP2 : MonoBehaviour
         return 0.3f * x + 0.2f;
     }
 
-    public float m_Width = 4;
-    public float m_Height = 4;
+    public float width = 4;
+    public float height = 4;
 
     void Start()
     {
@@ -27,8 +27,8 @@ public class SimplePerceptronP2 : MonoBehaviour
         //Make 2,000 training points.
         for (int i = 0; i < training.Length; i++)
         {
-            float x = Random.Range(-m_Width / 2, m_Width / 2);
-            float y = Random.Range(-m_Height / 2, m_Height / 2);
+            float x = Random.Range(-width / 2, width / 2);
+            float y = Random.Range(-height / 2, height / 2);
             //Is the correct answer 1 or -1?
             int answer = 1;
             if (y < f(x)) answer = -1;
@@ -39,7 +39,7 @@ public class SimplePerceptronP2 : MonoBehaviour
 
     void Update()
     {
-        ptron.train(training[count].inputs, training[count].answer);
+        ptron.Train(training[count].inputs, training[count].answer);
         //For animation, we are training one point at a time.
         count = (count + 1) % training.Length;
 
@@ -50,20 +50,20 @@ public class SimplePerceptronP2 : MonoBehaviour
     {
         for (int i = 0; i < count; i++)
         {
-            int guess = ptron.feedforward(training[i].inputs);
+            int guess = ptron.Feedforward(training[i].inputs);
             //Show the classification—no fill for -1, black for +1.
             Color col = Color.blue;
             if (guess > 0) col = Color.blue;
             else col = Color.red;            
             
             //Draw seperation line
-            var p1 = new Vector3(-m_Width / 2, f(-m_Height / 2), 0);
-            var p2 = new Vector3(m_Width / 2, f(m_Height / 2), 0);
+            var p1 = new Vector3(-width / 2, f(-height / 2), 0);
+            var p2 = new Vector3(width / 2, f(height / 2), 0);
             Gizmos.DrawLine(p1, p2);
 
             //Draw Perceptron seperation line
-            var p3 = new Vector3(-m_Width / 2, ptron.guessY(-m_Width / 2), 0);
-            var p4 = new Vector3(m_Width / 2, ptron.guessY(m_Width / 2), 0);
+            var p3 = new Vector3(-width / 2, ptron.GuessY(-width / 2), 0);
+            var p4 = new Vector3(width / 2, ptron.GuessY(width / 2), 0);
             Gizmos.DrawLine(p3, p4);
 
             // Draw result
